af5106059ba6aee329bac20701a8402e7d88caa4
paulandreacl <paula150995@hotmail.com>
2021-12-20 15:15:10 -0500

    routing entre index y about

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nabvar from '../component/navbar/Nabvar';
+import Link from 'next/link';
+
+const About = () => {
+    return (
+        <div>
+            <Nabvar/>
+            <p>Esta es la página de about</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nabvar from '../component/navbar/Nabvar';
+import Link from 'next/link';
+
+const Index = () => {
+    return (
+        <div>
+            <Nabvar/>
+            <p>Home</p>
+        </div>
+    )
+}
+
+export default Index;
